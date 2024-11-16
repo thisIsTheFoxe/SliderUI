9924a61d3396015196694ad1505615a63cbedb1c
Henry <thisisthefoxe@gmail.com>
2024-11-16 17:55:37 +1300

    Update SliderConstants.swift
    reduce default limitation to align with iOS system slider

--- Sources/SliderUI/SliderConstants.swift
+++ Sources/SliderUI/SliderConstants.swift
@@ -16,7 +16,7 @@ public struct SliderConstants {
     
     public init(widthScale: CGFloat = 2,
                 heightScale: CGFloat = 1.5,
-                limitation: CGFloat = 0.25,
+                limitation: CGFloat = 0.1,
                 stretchiness: CGFloat = 0.15,
                 scalieness: CGFloat = 0.25,
                 compactSize: CGFloat = 8) {
